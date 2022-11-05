2950bf970253206807a97bf3d1432e4683cfa72b
Eddy G <eddy.spambari@gmail.com>
2022-11-05 03:28:18 -0700

    added tab buttons and finished home page for now

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
 import './styles.css';
-import burgerIcon from "./images/burger.png";
 import {homePage} from "./modules/home";
 import {menuPage} from "./modules/menu";
 import {aboutPage} from "./modules/about";
+import {removeHomePage} from "./modules/home";
+homePage();
 
 const header = document.getElementById("header");
-    const headerTitle = document.createElement('h1');
-    headerTitle.textContent = "Ed's BITCHIN' Burgers";
+    const headerTitle = document.createElement('p');
     headerTitle.classList.add("headerTitle");
+    headerTitle.textContent = "Ed's BITCHIN' Burgers";
     header.appendChild(headerTitle);
-    
-const burgerPic = new Image();
-burgerPic.src = burgerIcon;
-burgerPic.classList.add("burgerPic");
-content.appendChild(burgerPic);
 
-homePage();
-aboutPage();
-menuPage();
+    const home = document.createElement('button');
+    home.classList.add("home");
+    home.textContent = "Home"
+    header.appendChild(home);
 
+    const menu = document.createElement('button');
+    menu.classList.add("menu");
+    menu.textContent = "Menu"
+    header.appendChild(menu);
+
+    const about = document.createElement('button');
+    about.classList.add("about");
+    about.textContent = "About"
+    header.appendChild(about);
+
+const homeBtn = document.querySelector(".home");     homeBtn.addEventListener('click', homePage);
+const menuBtn = document.querySelector(".menu");     menuBtn.addEventListener('click', menuPage);
+const aboutBtn = document.querySelector(".about");   aboutBtn.addEventListener('click', aboutPage);
 
 
 
